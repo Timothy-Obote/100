4374fc8dff8a6eb41c145be533ce4a662d241741
Tim <obotetimothy005@gmail.com>
2025-06-12 21:54:09 +0300

    Creating FAST APIs
    I created an api and tested using postman

---
+++ API/FastAPI/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# request Get method url: "/"
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    #rating: Optional[int] = None
+    
+my_posts = [{"title": "title of post 1", "content":"content of post 2", "id": 2},{"title":"favourite foods","content": "I like pizza", "id": 2 }]
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+
+@app.post("/posts")
+#def create_post(payload: dict = Body(...)):
+def create_posts(new_post: Post ):
+    print(new_post.published)
+    print(new_post.dict())
+    #return {"new-post" : f"title {payload['title']} content:{payload['content']}"}
+    return {"data": "new_post"}
+    #print(new_post) 
+    
+#title, str, content str, category, Bool published 
+
