f1850c6837621b1e2d03b1ec6096166793a1a2c7
Tim <obotetimothy005@gmail.com>
2025-06-13 18:36:03 +0300

    Update main.py
     basic FastAPI application that exposes a small API for handling blog-like posts

--- API/FastAPI/main.py
+++ API/FastAPI/main.py
@@ -1,6 +1,8 @@
+from random import randrange
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
-from pydantic import BaseModel
+from pydantic import BaseModel                       
 
 app = FastAPI()
 
@@ -10,9 +12,15 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    #rating: Optional[int] = None
+    rating: Optional[int] = None
     
 my_posts = [{"title": "title of post 1", "content":"content of post 2", "id": 2},{"title":"favourite foods","content": "I like pizza", "id": 2 }]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
 
 @app.get("/")
 def read_root():
@@ -25,12 +33,21 @@ def get_posts():
 
 @app.post("/posts")
 #def create_post(payload: dict = Body(...)):
+
 def create_posts(post: Post ):
-    print(post.published)
-    print(post.dict())
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0,1000000)
+    #print(post.published)
+    #print(post.dict())
     #return {"new-post" : f"title {payload['title']} content:{payload['content']}"}
-    return {"data": "post"}
+    my_posts.append(post_dict)
+    return {"data": post_dict}
     #print(new_post) 
     
 #title, str, content str, category, Bool published 
-
+@app.get("/posts/{id}")
+def get_post(id):
+    print(id)
+    post = find_post(id)
+    return{"post_detail": post}
+    
