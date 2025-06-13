0bc8edc014d0838d8996b05d6151e9ae081fbeac
Tim <obotetimothy005@gmail.com>
2025-06-13 22:20:54 +0300

    Update main.py
    delete

--- API/FastAPI/main.py
+++ API/FastAPI/main.py
@@ -75,8 +75,13 @@ def delete_post(id: int):
     #my_posts.pop(index)
     index = find_index_post(id)
     
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id:{id} does not exist")
+    
     my_posts.pop(index)
-    return{'message' : 'post was succesfully deleted'}
+    #return{'message' : 'post was succesfully deleted'}
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
     
