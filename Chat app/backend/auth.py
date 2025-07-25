f23bc1448599a4eb92a5bf4101d54d119296bcdc
Tim <obotetimothy005@gmail.com>
2025-07-25 12:08:30 +0300

    Create auth.py

---
+++ Chat app/backend/auth.py
@@ -0,0 +1,18 @@
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from fastapi import HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime, timedelta
+
+SECRET_KEY = "your_secret"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
