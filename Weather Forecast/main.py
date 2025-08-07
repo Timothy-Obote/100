a7ea7644c575053e87b1f8fd0187ffcfaba006b7
Tim <obotetimothy005@gmail.com>
2025-08-07 21:36:00 +0300

    Update main.py

--- Weather Forecast/main.py
+++ Weather Forecast/main.py
@@ -1,8 +1,14 @@
 import requests
 
+import os
+from twilio.rest import Client
+
 OMW_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 
 api_key = "643edc50fddd59596c9720c56229408d"
+
+account_sid = "AC8d433276b8644e9491215ac5c34deccd"
+auth_token = "871fe7efb4a2240edccd3bb126b95394"
 
 weather_params = {
     "lat":-1.292066,
@@ -24,4 +30,12 @@ for hour_data in weather_data["list"]:
     if int(condition_code) < 700 :
         will_rain = True
 if will_rain:
-        print("Bring an umbrella.")
+        client = Client(account_sid, auth_token)
+        
+        message = client.messages.create(
+    body="It will rain ,bring an umbrella",
+    from_="+18566060195",
+    to="+18566060195",
+)
+
+print(message.status)
